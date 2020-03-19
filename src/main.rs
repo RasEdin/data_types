2ddc99e98db5d022e7e6c57302ce18b452128212
RasEdin <aable24@gmail.com>
2020-03-19 17:28:49 +0100

    Integers, signed and unsigned.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+  // 
   let guess: u32 = "42".parse().expect("Not a number!");
   println!("This guess is: {}", guess);
+
+  // Intergers
+  let val_signed32: i32 = -55;
+  let val_unsigned32: u32 = 55;
+
+  println!("Signed integer: {}", val_signed32);
+  println!("Unsigned integer: {}", val_unsigned32);
 }
