e158120ff2d6d68310b5d9f7319e1b796036f7a9
RasmusEdinCode@gmail.com <RasmusEdinCode@gmail.com>
2020-07-06 19:24:51 +0200

    Going through a few more things
    tuples, arrays, functions, return values, fixing errors.

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,67 @@ fn main() {
   // Numeric operators
   let sum = 3f64 / 1.5;
   println!("Sum of 3 / 1.5: {}", sum);
+
+  // Boolean
+  let t = true;
+
+  let f: bool = false; // Explicit type annotation
+  println!("t is: {} and f is: {}", t, f);
+  // tuples
+  let tup = (500, 36, 25);
+
+  let (x, y, _z) = tup;
+
+  println!("The value of y is {} and the value of x is {}", y, x);
+  
+  let tup2 = tup.2;
+  println!("The third value in the tuple should be 25, it is: {}", tup2);
+
+  // Arrays
+  let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+  let a: [i32; 5] = [1, 2, 3, 4, 5]; // Explicit type defined, length of five
+
+  let _first = a[0]; // Underscore at a variable name makes the variable not neccesary to be called.
+  let _second = a[1];
+
+  println!("month 2 is: {}", months[1]);
+  
+  // functions
+  let function_parameter: i32 = 64;
+  let second_parameter: i32 = 46;
+  a_second_function(function_parameter, second_parameter);
+  bindingfunction();
+  println!("The function five() returns: {}",five());
+  println!("Add one to value: {}, {}", 6, plus_one(6));
+
+  // Control structures
+}
+
+// function 1 parameter signed 32-bit integer 
+fn a_second_function(x: i32, y: i32)
+{
+  println!("Called the second function");
+  println!("The value passed to the function is: {}", x);
+  println!("The 2nd value passed to the function is: {}", y);
+}
+
+fn bindingfunction()
+{
+  let _x = 5;
+
+  let y = {
+      let x = 3;
+      x + 1
+  };
+
+  println!("The value of y is: {}", y);
+}
+// Function with return type
+fn five() -> i32 { 
+  return 5
+}
+
+fn plus_one(x: i32) -> i32 {
+  return x + 1
 }
